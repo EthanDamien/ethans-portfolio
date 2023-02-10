82b46ddd62f90a27bebc11f126ac600af81d18c3
Ethan Damien <ethanchang.d@gmail.com>
2023-02-10 17:52:55 -0500

    Added more cards

--- src/sections/Projects/Projects.js
+++ src/sections/Projects/Projects.js
@@ -11,6 +11,8 @@ const Project = () => {
         </div>
         <div className='carousel'>
           {expCard("omw")}
+          {expCard("retrotable")}
+          {expCard("glimpse")}
         </div>
       </header>
     )
@@ -43,11 +45,27 @@ function expCard(type) {
           <img src={omwlogo} className="cardLogo" alt="logo" />
           </div>
           <div className='expCardContent'>
-              <h1>OMW (On My Way)</h1>
-              <p className='gradientOrangeStatic'>The Startup Idea</p>
-              <p>The OMW app idea served as a starting point to learn how to research and blueprint an idea. During the process I learned to take executive initiative and push my creative limits.</p>
+              <h1>Retrotable</h1>
+              <p className='gradientOrangeStatic'>The AI Study App</p>
+              <p>Combining the ideas of Spaced-Repetition, Supermemo-2, and OpenAI's ChatGPT. We are innovating on making student's lives easier in the new climate of AI assistance.</p>
               <p>
                 <a href='https://www.smalljuice.com' className='hvr-shrink gradientOrangeStatic'>Read more about the project here.</a>
+              </p>
+          </div>
+          </div> 
+          )
+      case 'glimpse':
+        return (
+          <div className='ProjectCard'>
+          <div className='imgContain'>
+          <img src={omwlogo} className="cardLogo" alt="logo" />
+          </div>
+          <div className='expCardContent'>
+              <h1>Glimpse</h1>
+              <p className='gradientOrangeStatic'>FaceID for Small Employers</p>
+              <p>A proof of concept Clock-in/Clock-out app which uses AWS Rekognition and full cloud implementation. Meant for small businesses, prioritizing non-contact convenience.</p>
+              <p>
+                <a href='https://www.smalljuice.com' className='hvr-shrink gradientOrangeStatic'>Check out our University Project here.</a>
               </p>
           </div>
           </div> 
